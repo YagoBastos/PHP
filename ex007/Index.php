11e6d5d683ea530a7d9c04c5f874ab0a9547eb99
Yago Bastos <Yagobastos02102004@gmail.com>
2023-03-15 14:02:29 -0300

    atualização desafio 1 e desafio 3

---
+++ ex007/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas v1.0</h1>
+        <form action="fun.php" method="post">
+            <legend>Quantos R$ você tem na carteira?:</legend>
+            <input type="number" name="numero" id="idnumero">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+</body>
+</html>
--- ex007/fun.php
+++ ex007/fun.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio PHP 1</title>
+    <title>Desafio PHP 3</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
     <h1>Resultado final</h1>
         <?php 
-            $numero = $_POST["numero"];
-            echo "O numero é: $numero";
-            echo "</br>O antecessor do numero é: ".($numero-1);
-            echo "</br>O sucessor do numero é: ".($numero+1);
+            $numero = $_POST["numero"] ?? "Numero NÃO informado";
+            echo "R$: $numero";
+            $numero = $numero/5.33;
+            echo "</br>US$: $numero";
         ?>
         <input type="button" value=" &#x25C0; Voltar" onclick="javascript:history.go(-1)">
     </main>
